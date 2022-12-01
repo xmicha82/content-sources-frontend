5ede8893d1cfcaafe7539a713d745682a73fec34
Andrew <andrewgdewar@gmail.com>
2022-12-01 13:18:17 -0700

    Fixes 295: UI crash caused by chrome update in parent (#73)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import NotificationsPortal from '@redhat-cloud-services/frontend-components-noti
 import Routes from './Routes';
 import { useAppContext } from './middleware/AppContext';
 import { NoPermissionsPage } from './components/NoPermissionsPage/NoPermissionsPage';
-import { useNavigate } from 'react-router-dom';
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 import { insights } from '../package.json';
 
@@ -18,29 +17,17 @@ const { appname } = insights;
 
 export default function App() {
   const { rbac, setRbac } = useAppContext();
-  const navigate = useNavigate();
   const chrome = useChrome();
 
   useEffect(() => {
-    let unregister;
-    if (chrome && !unregister) {
-      // Get chrome and register app
-      const registry = getRegistry();
-      registry.register({ notifications: notificationsReducer });
-      const { updateDocumentTitle, on: onChromeEvent } = chrome.init();
-      updateDocumentTitle(appname);
-      unregister = onChromeEvent('APP_NAVIGATION', (event) => navigate(`/${event.navId}`));
-    }
+    // Get chrome and register app
+    const registry = getRegistry();
+    registry.register({ notifications: notificationsReducer });
 
     if (chrome && !rbac) {
       // Get permissions and store them in context
       chrome.auth.getUser().then(async () => fetchRBAC(appname).then(setRbac));
     }
-
-    return () => {
-      unregister();
-    };
-    // This ensures that we only run this once, on a change from falsey > truthy
   }, [!!chrome]);
 
   switch (true) {
