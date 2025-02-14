582e8a8f28a2d0fa3451e8d2dde3477b47f6e6fc
Bryttanie <28575816+xbhouse@users.noreply.github.com>
2025-02-14 12:35:41 -0500

    HMS-5426: add PW test for RH repos (#424)
    HMS-5426: add rh repo test

---
+++ _playwright-tests/UI/RedHatRepo.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from '@playwright/test';
+import { navigateToRepositories } from './helpers/navHelpers';
+import { closePopupsIfExist } from './helpers/helpers';
+
+test.describe('Red Hat Repositories', () => {
+    const smallRHRepo = 'Red Hat Ansible Engine 2 for RHEL 8 x86_64 (RPMs)'
+
+    test.beforeEach(async ({ page }) => {
+        await test.step('Navigate to repositories page', async () => {
+            await navigateToRepositories(page);
+            await closePopupsIfExist(page);
+        });
+        await test.step('Navigate to Red Hat repositories', async () => {
+            await page.getByRole('button', { name: 'Red Hat' }).click();
+        });
+    });
+
+    test('Wait for repository status to be "Valid"', async ({ page }) => {
+        test.setTimeout(600000);
+        const status = page
+            .getByRole('row')
+            .filter({ hasText: smallRHRepo })
+            .getByRole('gridcell')
+            .filter({ hasText: 'Valid' });
+        
+        await test.step('Filter repos by name', async () => {
+            await page.getByPlaceholder('Filter by name/url').fill(smallRHRepo);
+        });
+
+        await test.step('Wait for a valid status', async () => {
+            await status.waitFor({ state: 'visible' });
+        });
+    });
+
+    test('Check repository snapshots', async ({ page }) => {
+        await test.step('Open the snapshots list modal', async () => {
+            await page
+                .getByRole('row')
+                .filter({ hasText: smallRHRepo })
+                .getByLabel('Kebab toggle')
+                .click();
+            await expect(page.getByRole('menuitem', { name: 'View all snapshots' })).toBeVisible();
+            await page.getByRole('menuitem', { name: 'View all snapshots' }).click();
+            await expect(page
+                .getByTestId('snapshot_list_modal')
+                .filter({ hasText: smallRHRepo }))
+                .toBeVisible();
+        });
+
+        await test.step('Close the snapshots list modal', async () => {
+            // Using testId here because the modal has 2 close buttons
+            await page.getByTestId('snapshot_list_modal-ModalBoxCloseButton').click();
+        });
+    });
+});
