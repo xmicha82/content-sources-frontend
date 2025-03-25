bf1f79dca083c60c2a10821df68f6dffd1665d8d
swadeley <swadeley@redhat.com>
2025-03-25 15:18:36 +0000

    HMS-5648: Add GPG key test
    * HMS-5648: Add GPG key test

---
+++ _playwright-tests/UI/GPG_keys.spec.ts
@@ -0,0 +1,68 @@
+import { test, expect } from '@playwright/test';
+import { navigateToRepositories } from './helpers/navHelpers';
+import { deleteAllRepos } from './helpers/deleteRepositories';
+import { closePopupsIfExist, getRowByNameOrUrl } from './helpers/helpers';
+
+const repoNamePrefix = 'GPG-key';
+const randomName = () => (Math.random() + 1).toString(36).substring(2, 6);
+const repoName = `${repoNamePrefix}-${randomName()}`;
+
+const url = 'https://jlsherrill.fedorapeople.org/fake-repos/signed/';
+const packages_key =
+  'http://jlsherrill.fedorapeople.org/fake-repos/needed-errata/RPM-GPG-KEY-dummy-packages-generator';
+const meta_key = 'https://jlsherrill.fedorapeople.org/fake-repos/signed/GPG-KEY.gpg';
+
+test.describe('Test GPG keys', () => {
+  test('Create a repo, add a GPG Key, toggle metadata', async ({ page }) => {
+    await test.step('Delete any GPG key test repos that exist', async () => {
+      await navigateToRepositories(page);
+      await closePopupsIfExist(page);
+      await deleteAllRepos(page, `&search=${repoNamePrefix}`);
+    });
+    await test.step('Create a repository', async () => {
+      // Click on the 'Add repositories' button
+      // HMS-5268 There are two buttons on the ZeroState page
+      await page.getByRole('button', { name: 'Add repositories' }).first().click();
+      await expect(page.getByRole('dialog', { name: 'Add custom repositories' })).toBeVisible();
+      // Fill in the repository details
+      await page.getByLabel('Name').fill(`${repoName}`);
+      await page.getByLabel('Introspect only').click();
+      await page.getByLabel('URL').fill(url);
+      await page.getByPlaceholder('Paste GPG key or URL here').fill(packages_key);
+      expect(page.getByRole('textbox', { name: 'gpgkey_file_to_upload' })).toContainText(
+        '-----BEGIN PGP PUBLIC KEY BLOCK-----',
+      );
+      // Check that validate fails if you select the metadata option
+      await page.getByText('Package and metadata').click();
+      await expect(page.getByText('Error validating signature:')).toBeVisible();
+      await expect(page.getByRole('button', { name: 'Save', exact: true })).toBeDisabled();
+      await page.getByText('Package verification only').click();
+      // Save button would be disabled for bad or incorrect gpg key
+      await expect(page.getByRole('button', { name: 'Save', exact: true })).toBeEnabled;
+      await page.getByPlaceholder('Paste GPG key or URL here').fill('I am not a GPG Key');
+      await expect(page.getByText('Error validating signature:')).toBeVisible();
+      await expect(page.getByRole('button', { name: 'Save', exact: true })).toBeDisabled();
+      await page.getByPlaceholder('Paste GPG key or URL here').fill(packages_key);
+      await page.getByRole('button', { name: 'Save', exact: true }).click();
+    });
+    await test.step('Change to Metadata GPG Key', async () => {
+      // Search for the created repo
+      const row = await getRowByNameOrUrl(page, repoName);
+      await expect(row.getByText('Valid')).toBeVisible({ timeout: 60000 });
+      // Open edit modal
+      await row.getByLabel('Kebab toggle').click();
+      await row.getByRole('menuitem', { name: 'Edit' }).click();
+      await expect(page.getByRole('dialog', { name: 'Edit custom repository' })).toBeVisible();
+      await page.getByPlaceholder('Paste GPG key or URL here').fill(meta_key);
+      expect(page.getByRole('textbox', { name: 'gpgkey_file_to_upload' })).toContainText(
+        '-----BEGIN PGP PUBLIC KEY BLOCK-----',
+      );
+      await page.getByText('Package and metadata').click();
+      // Save button would be disabled for bad or incorrect gpg key
+      await page.getByRole('button', { name: 'Save changes', exact: true }).click();
+    });
+    await test.step('Post test cleanup', async () => {
+      await deleteAllRepos(page, `&search=${repoNamePrefix}`);
+    });
+  });
+});
